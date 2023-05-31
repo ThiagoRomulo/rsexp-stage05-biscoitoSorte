e405dddffe0f12c2cebc7c554535297e63ff7134
ThiagoRomulo <63716510+ThiagoRomulo@users.noreply.github.com>
2023-05-31 18:36:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Criação de variáveis
+const messages = [
+  "Procure acender uma vela em vez de amaldiçoar a escuridão",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+  "A vida sempre será finita, porém qual a diferença de sorrirmos ou chorarmos hoje. Sorria!",
+]
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnCookie = document.querySelector("#closedCookie")
+const btnOther = document.querySelector("button")
+const textMsg = screen2.querySelector("p")
+let index = 0;
+
+// Eventos
+btnCookie.addEventListener('click', newMsg)
+btnOther.addEventListener('click', nextMsg)
+
+// Funções
+function newMsg() {
+  textMsg.innerText = messages[index]
+  toggleScreen()
+}
+
+function nextMsg() {
+  if (index < 2){
+    index++
+  } else {
+    index = 0
+  }
+  
+  toggleScreen()
+}
+
+function toggleScreen () {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
